c713a36637578475b8484ce2c0ae71f6517cdbed
Cooli <dishan@bluewin.ch>
2025-09-10 09:06:30 +0200

    fix: test comment push

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,7 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-//testKommentar
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
